798e417e146c921484f448113ede9bc792316565
KNFullStack <knfullstack@gmail.com>
2022-07-26 21:05:41 +0000

    product management template page added

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('product/', views.product_detail, name="product_detail")
+    path('product/', views.product_detail, name="product_detail"),
+    path('product/admin/', views.product_management, name="product_management"),
 ]
--- products/views.py
+++ products/views.py
@@ -15,3 +15,10 @@ def product_detail(request):
     Returns the product details page
     """
     return render(request, "products/product_detail.html")
+
+
+def product_management(request):
+    """
+    Returns the product management page
+    """
+    return render(request, "products/product_management.html")
