caa867134edb4bc6524d19a83c9b04d5bbb74366
KNFullStack <knfullstack@gmail.com>
2022-08-17 14:53:48 +0000

    added crispy forms to allauth - profile model started

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    phone_number = models.CharField(max_length=50, null=False, blank=False)
+    postcode = models.CharField(max_length=50, null=True, blank=False)
+    town_city = models.CharField(max_length=50, null=False, blank=False)
+    address_one = models.CharField(max_length=100, null=False, blank=False)
+    address_two = models.CharField(max_length=100, null=False, blank=False)
+    county = models.CharField(max_length=50, null=True, blank=True)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
--- subscription/forms.py
+++ subscription/forms.py
@@ -9,20 +9,23 @@ class SubscriptionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         placeholders = {
-            "name" : "Full Name",
-            "email" : "Email Address",
-            "phone_number" : "Phone Number",
-            "address_one" : "Street Address 1",
-            "address_two" : "Street Address 2",
-            "town_city" : "Town or City",
-            "county" : "County",
-            "postcode" : "Postcode",
-            "country" : "Country",
+            "name": "Full Name",
+            "email": "Email Address",
+            "phone_number": "Phone Number",
+            "address_one": "Street Address 1",
+            "address_two": "Street Address 2",
+            "town_city": "Town or City",
+            "county": "County",
+            "postcode": "Postcode",
+            "country": "County",
 
         }
+
+        self.fields["name"].widget.attrs["autofocus"] = True
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f"{placeholders[field]} *"
             else:
                 placeholder = placeholders[field]
-        self.fields["name"].widget.attrs["autofocus"] = True
+            self.fields[field].widget.attrs["placeholder"] = placeholder
+            self.fields[field].label = False
