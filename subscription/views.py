f1a9d296fc62c4230dcbe62031d6ef177aa47405
KNFullStack <knfullstack@gmail.com>
2022-08-19 14:50:28 +0000

    attempts at next form page

--- subscription/views.py
+++ subscription/views.py
@@ -17,17 +17,14 @@ def subscribe(request):
     return render(request, "subscription/subscription.html", context)
 
 def subscribe_details(request):
-# need to get the current subscription ID and then pass that to the next page to be used as the choice for the subscription
     if request.method == "POST":
         subscription_details_form = SubscriptionDetailsForm(request.POST)
         if subscription_details_form.is_valid():
             # chocolate_quantity = subscription_details_form.cleaned_data['chocolate_quantity']
-            # banana_quantity = subscription_details_form.cleaned_data['banana_quantity']
-            # strawberry_quantity = subscription_details_form.cleaned_data['strawberry_quantity']
-            # cookies_quantity = subscription_details_form.cleaned_data['cookies_quantity']
             subscription_details_form.save()
-            # print(add_bag_quantites(chocolate_quantity, banana_quantity, strawberry_quantity, cookies_quantity))
-            return redirect(reverse('subscribe_items'))
+            request.session['subscription_number'] = subscription_details_form.instance.subscription_number
+            # return redirect(reverse('subscribe_items'))
+            return render(request, 'subscription/subscription_items.html', {'subscription_number': request.session['subscription_number']})
 
     subscription_details_form = SubscriptionDetailsForm()
     context = {
@@ -39,8 +36,12 @@ def subscribe_details(request):
 def subscribe_items(request):
 # this formed rendered below like i thought, but need to get a custom form perhaps and add the quantities there? e.g. only shows 1 product box
 # look into the subform / nested form stuff?
-    subscription_items_form = SubscriptionItemsForm()
+    instance_details = {
+        "subscription": 1,
+    }
+    subscription_items_form = SubscriptionItemsForm(initial=instance_details)
     context = {
         "subscription_items_form": subscription_items_form,
     }
+    print(request.session["subscription_number"])
     return render(request, "subscription/subscription_items.html", context)
