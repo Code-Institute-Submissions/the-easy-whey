08174709b67773e51b89e01af6f3467a08c85498
KNFullStack <knfullstack@gmail.com>
2022-08-25 09:38:04 +0000

    images added to product model, directories set up

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.CharField(max_length=200, null=False, blank=False)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=False, blank=False, default="12.99")
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='products')
 
     def __str__(self):
         return self.flavour
