6d41e8d054ff4f6e9c0068bfb5a3051040b0c9a0
KNFullStack <knfullstack@gmail.com>
2022-09-02 19:09:06 +0000

    stripe installed and environment vars added

--- easy_whey/settings.py
+++ easy_whey/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from os import path
+if path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jh0xzzzni*#i37_o&(g-x0=gyt4d3!-uu%ws3l--5=23asfl@!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_PRODUCT_KEY = os.environ.get('STRIPE_PRODUCT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
