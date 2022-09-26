c2fb2113683c0bf4ad72f0001a99a800df43ebad
KNFullStack <knfullstack@gmail.com>
2022-09-26 11:30:36 +0000

    removal of test validation function

--- contact/forms.py
+++ contact/forms.py
@@ -7,8 +7,8 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = "__all__"
     
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        if "Kingsley" not in name:
-            raise ValidationError("You have forgotten about Kingsley!")
-        return name
+    # def clean_name(self):
+    #     name = self.cleaned_data['name']
+    #     if "Kingsley" not in name:
+    #         raise ValidationError("You have forgotten about Kingsley!")
+    #     return name
