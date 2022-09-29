eb677fad66356152fe75bea92ca8dce60ea1e886
KNFullStack <knfullstack@gmail.com>
2022-09-29 11:23:11 +0000

    contact app automated testing started

---
+++ contact/test_models.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from django.test import Client
+from .models import Contact
+from .forms import ContactForm
+# Create your tests here.
+
+
+class ModelTestCase(TestCase):
+    """
+    Test object model object creation
+    """
+    def setUp(self):
+        self.contact_message = Contact.objects.create(
+            name="TestName",
+            email="testemail@email.com",
+            phone_number="07123123123",
+            message="Test message."
+        )
+
+    def test_message_details_name(self):
+        self.assertEqual(self.contact_message.name, "TestName")
+
+    def test_message_details_email(self):
+        self.assertEqual(self.contact_message.email, "testemail@email.com")
+
+    def test_message_details_phone_number(self):
+        self.assertEqual(self.contact_message.phone_number, "07123123123")
+
+    def test_message_details_message(self):
+        self.assertEqual(self.contact_message.message, "Test message.")
+
+    def test_model_string_name(self):
+        self.assertEqual(str(self.contact_message),
+                         "Message received from TestName, testemail@email.com")
+
+    def test_model_phone_number_invalid(self):
+        c = Client()
+        response = c.post('/contact/', {
+            'name': 'TestName',
+            'email': 'testemail@email.com',
+            'phone_number': '0712312312',
+            'message': 'Test message.',
+            }
+        )
+        errors = response.context["form"].errors
+        self.assertEqual(errors["phone_number"][0], "Phone number must be entered in the format: 07xxxxxxxxx")
---
+++ contact/test_urls.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from .models import Contact
+
+# Create your tests here.
+
+
+class ContactURLTestCaseNonSuperUser(TestCase):
+    """
+    Test urls are returning appropriate responses - non superuser,
+    non-super users get redirected to login page
+    """
+    def setUp(self):
+        self.c = Client()
+
+    def test_url_contact(self):
+        response = self.c.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Please fill out the form below.")
+
+    def test_url_contact_admin(self):
+        response = self.c.get('/contact/admin/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_url_contact_admin_message(self):
+        response = self.c.get('/contact/admin/1')
+        self.assertEqual(response.status_code, 302)
+
+    def test_url_contact_admin_delete(self):
+        response = self.c.get('/contact/admin/delete/1')
+        self.assertEqual(response.status_code, 302)
+    
+
+class ContactURLTestCaseSuperUser(TestCase):
+    """
+    Test urls are returning appropriate responses - superuser
+    """
+
+    def setUp(self):
+        self.c = Client()
+        self.my_admin = User.objects.create_superuser('myuser', 'testemail@email.com', "password")
+        self.c.login(username=self.my_admin.username, password="password")
+
+    def test_url_contact_admin_no_messages(self):
+        response = self.c.get('/contact/admin/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no messages!")
+        self.assertNotContains(response, "From")
+
+    def test_url_contact_admin_with_message(self):
+        self.contact_message = Contact.objects.create(
+            name="TestName",
+            email="testemail@email.com",
+            phone_number="07123123123",
+            message="Test message."
+        )
+        response = self.c.get('/contact/admin/')
+        self.assertContains(response, "TestName")
+        self.assertContains(response, "testemail@email.com")
+        self.assertContains(response, "Utilities")
+
+    def test_url_contact_admin_with_message_view_message(self):
+        self.contact_message = Contact.objects.create(
+            name="TestName",
+            email="testemail@email.com",
+            phone_number="07123123123",
+            message="Test message."
+        )
+        response = self.c.get(f'/contact/admin/{self.contact_message.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Message from:</strong> TestName.")
+        self.assertContains(response, "Email:</strong> testemail@email.com.")
+        self.assertContains(response, "Phone:</strong> 07123123123")
+        msg = "Message:</strong></p>\n                <p>Test message."
+        self.assertContains(response, msg)
